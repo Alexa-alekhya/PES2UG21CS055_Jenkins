12c8d08e725eb1650d5c33e06d0138e55709a4f4
unknown <ponnekanti.alekhya@gmail.com>
2024-03-05 22:01:41 +0530

    Describe by PES2UG21CS055

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run, SRN: PES2UG21CS055\n";
+  cout << "I have made change which needs to be seen on Jenkins\n";
   return 0;
 }
